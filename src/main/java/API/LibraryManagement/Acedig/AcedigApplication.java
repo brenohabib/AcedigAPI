da24e384652044f40d69b915e34869984df65189
habs <brenohabib2013@gmail.com>
2025-05-30 22:04:00 -0300

    fist commit

---
+++ src/main/java/API/LibraryManagement/Acedig/AcedigApplication.java
@@ -0,0 +1,13 @@
+package API.LibraryManagement.Acedig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcedigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcedigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API/LibraryManagement/Acedig/AcedigApplicationTests.java
@@ -0,0 +1,13 @@
+package API.LibraryManagement.Acedig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcedigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

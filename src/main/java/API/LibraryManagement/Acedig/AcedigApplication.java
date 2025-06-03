b9df3be177a7aedc21794cde0695bc89851cfe36
habs <brenohabib2013@gmail.com>
2025-06-02 23:17:23 -0300

    Configuration files

--- src/main/java/API/LibraryManagement/Acedig/AcedigApplication.java
+++ src/main/java/API/LibraryManagement/Acedig/AcedigApplication.java
@@ -2,8 +2,11 @@ package API.LibraryManagement.Acedig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AcedigApplication {
 
 	public static void main(String[] args) {

1c94a31e56cd18a1578281cb996f7ff70514b382
habs <brenohabib2013@gmail.com>
2025-06-22 11:09:00 -0300

    Moved Cors to another class

--- src/main/java/API/LibraryManagement/Acedig/AcedigApplication.java
+++ src/main/java/API/LibraryManagement/Acedig/AcedigApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,18 +16,5 @@ public class AcedigApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AcedigApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*");
-            }
-        };
     }
 }
